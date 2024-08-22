5c690f506e50f5f512c1e2f040ace6aa5a5352d7
Guilherme Nunes Lopes <gui.nunes253@gmail.com>
2024-08-22 19:29:59 -0300

    Update main.py

--- TP4/main.py
+++ TP4/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -10,8 +11,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, classification_report
-
+from sklearn.metrics import confusion_matrix, classification_report,f1_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score 
 # Carregar dados
 try:
     df = pd.read_csv('koi_data.csv')
@@ -27,6 +28,10 @@ df.info()
 X = df.iloc[:, 2:].values  # Todas as colunas exceto a primeira (KOI ID) e a segunda (label)
 Y = df.iloc[:, 1].values   # Segunda coluna (label) Koi Disposition, Confirmed, False Positive
 
+all_metrics=[]
+all_confusion_matrix=[]
+num_classes = len(np.unique(Y))  # Calcula o número de classes com base nos dados
+
 # Usando KFold para Validação Cruzada com k igual a 5.
 models = {
     'Naive Bayes': GaussianNB(),
@@ -35,14 +40,7 @@ models = {
     'Support Vector Machines': SVC(),
     'Random Forest': RandomForestClassifier(),
     'Gradient Tree Boosting': GradientBoostingClassifier(),
-    # 'Multi-layer Perceptron': MLPClassifier()  # Aumentar o número de iterações
-    # 'Multi-layer Perceptron': MLPClassifier(
-    #     hidden_layer_sizes=(25,),
-    #     activation='tanh',
-    #     solver='adam',
-    #     learning_rate_init=0.003,
-    #     max_iter=1000
-    # )
+    # 'Multi-layer Perceptron': definida dentro do escopo da funcao
 }
 
 def initial_Count():
@@ -99,35 +97,100 @@ def acuracia_media():
 
     plt.show()
 
-def plot_confusion_matrix(cm, title='Matriz de Confusão'):
-    fig, ax = plt.subplots()
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', ax=ax, cbar=False, 
-                xticklabels=['Confirmed', 'False Positive'], yticklabels=['Confirmed', 'False Positive'])
-    ax.set_xlabel('Predito')
-    ax.set_ylabel('Real')
-    ax.set_title(title)
-    plt.show()
+
 
 def evaluate_model(model, X, Y, kf):
     y_true = []
     y_pred = []
+    accuracies=[]
+    precisions = []
+    recalls = []
+    f1_scores = []
 
     # Realizando a validação cruzada e coletando as predições
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        Y_train, Y_test = Y[train_index], Y[test_index]
-        
-        pipeline = make_pipeline(StandardScaler(), model)
-        pipeline.fit(X_train, Y_train)
-        Y_pred = pipeline.predict(X_test)
-        
-        y_true.extend(Y_test)
-        y_pred.extend(Y_pred)
+    #validaçao cruzada k-fold com k igual a 5.
+    #train_index: Representa os índices dos dados que foram utilizados para treinar o modelo nessa iteração.
+    #test_index: Representa os índices dos dados que foram utilizados para testar o modelo nessa iteração.
+    #X[train_index]: Essa parte do código seleciona as linhas (amostras) da matriz X cujos índices estão presentes no array train_index. 
+    #Y[train_index]: Analogamente, seleciona os rótulos correspondentes às amostras de treinamento.
+    #X[test_index] e Y[test_index] fazem a mesma coisa, mas para os índices presentes em test_index, ou seja, selecionam as features e rótulos das amostras que serão usadas para testar o modelo.
 
-    # Calculando e exibindo a matriz de confusão
-    cm = confusion_matrix(y_true, y_pred)
-    print("Matriz de Confusão:")
-    plot_confusion_matrix(cm, title="Matriz de Confusão de "+str(model))
+
+    #make_pipeline: Esta função cria um objeto Pipeline do scikit-learn.
+    #StandardScaler(): Esta é uma classe que realiza o escalonamento de dados (normalização).
+    #model: É um placeholder para o seu modelo de aprendizado de máquina (por exemplo, LogisticRegression(), RandomForestClassifier() etc.).
+
+    #pipeline.fit(X_train, Y_train) Este comando treina o pipeline inteiro usando os dados de treinamento.
+    #.fit() serve para ajustar um modelo aos dados de treinamento.
+    #.fit() recebe como primeiro parametro os indices das linhas dos parametros 
+    #e como segundo parametro os mesmos indices das linhas dos resultados
+    #A função .predict() é usada para fazer previsões com um modelo de aprendizado de máquina que já foi treinado.
+    #.predict recebe os valores de teste, aplica o modelo, e retorna um Y esperado
+    #O objetivo da validação cruzada é comparar Y_pred com Y_test para avaliar a precisão do modelo. Se as previsões estiverem próximas dos rótulos reais, significa que o modelo generaliza bem para novos dados.
+    cm=np.zeros((num_classes, num_classes))
+    
+    for train_index, test_index in kf.split(X):
+       
+         X_train, X_test = X[train_index], X[test_index]
+         Y_train, Y_test = Y[train_index], Y[test_index]
+        
+         pipeline = make_pipeline(StandardScaler(), model)
+         pipeline.fit(X_train, Y_train)
+         
+         Y_pred = pipeline.predict(X_test)
+         y_true.extend(Y_test)# LENTO!
+         y_pred.extend(Y_pred)#LENTO!
+
+
+         accuracy = accuracy_score(Y_test, Y_pred)
+         aux_cm=confusion_matrix(Y_test,Y_pred)
+         cm+=aux_cm
+        
+         #formatted_accuracy = float(f"{accuracy:.5f}")
+         
+            
+         accuracy = accuracy_score(Y_test, Y_pred)
+         precision = precision_score(Y_test, Y_pred, average='weighted')
+         recall = recall_score(Y_test, Y_pred, average='weighted')
+         f1 = f1_score(Y_test, Y_pred, average='weighted')
+        
+         accuracies.append(round(accuracy, 4))
+         precisions.append(round(precision, 4))
+         recalls.append(round(recall, 4))
+         f1_scores.append(round(f1, 4))
+    
+         
+    
+    
+
+    
+    mean_accuracy = sum(accuracies) / len(accuracies)
+    mean_accuracy = sum(accuracies) / len(accuracies)
+    mean_precision = sum(precisions) / len(precisions)
+    mean_recall = sum(recalls) / len(recalls)
+    mean_f1 = sum(f1_scores) / len(f1_scores)
+    all_confusion_matrix.append(cm)
+    all_metrics.append((str(model), round(mean_accuracy, 4), round(mean_precision, 4), round(mean_recall, 4), round(mean_f1, 4)))
+    #df = pd.DataFrame({'Fold': range(1, 5 + 1), 'Acurácia': accuracies})
+
+    # Adiciona uma linha com a acurácia média
+    #df.loc[len(df)] = ['Acurácia Média',round(mean_accuracy,4) ]
+
+    # Plota a tabela
+    # plt.figure(figsize=(5, 5))
+    # plt.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
+    # plt.axis('off')
+    # plt.title('Acurácia por Fold e Média para'+str(model))
+    # plt.show()
+
+         #Para plotar uma matriz de confusão para cada Avaliação KFold
+         #cm = confusion_matrix(y_true, y_pred)
+         #plot_confusion_matrix(cm, title="Matriz de Confusão de "+str(model))
+
+    # # Calculando e exibindo a matriz de confusão
+    # cm = confusion_matrix(y_true, y_pred)
+    # print("Matriz de Confusão:")
+    # plot_confusion_matrix(cm, title="Matriz de Confusão de "+str(model))
 
     # Calculando e exibindo o relatório de métricas
     #cr = classification_report(y_true, y_pred, target_names=['False Positive', 'Confirmed'])
@@ -140,7 +203,7 @@ def naive_bayes_experiment(X, Y, kf):
     pipeline = make_pipeline(StandardScaler(), model)
     scores = cross_val_score(pipeline, X, Y, cv=kf, scoring='accuracy')
     mean_accuracy = scores.mean()
-    print(f'Naive Bayes: Acurácia média = {mean_accuracy:.4f}, Desvio padrão = {scores.std():.4f}')
+    #print(f'Naive Bayes: Acurácia média = {mean_accuracy:.4f}, Desvio padrão = {scores.std():.4f}')
     return mean_accuracy
 
 def decision_tree_experiment(X, Y, kf):
@@ -240,19 +303,70 @@ def mlp_experiment(X, Y, kf):
     plt.grid(True)
     plt.show()
 
-def main():
-    initial_Count()  # Certifique-se de que essa função está definida
-    acuracia_media()
 
-    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+
+def results():
+    kf = KFold(n_splits=5, shuffle=True,random_state=42)
 
     naive_bayes_experiment(X, Y, kf)
     svm_experiment(X, Y, kf)
-    decision_tree_experiment(X, Y, kf)
-    knn_experiment(X, Y, kf)
-    random_forest_experiment(X, Y, kf)
-    # mlp_experiment(X, Y, kf)
+    #decision_tree_experiment(X, Y, kf)
+    #knn_experiment(X, Y, kf)
+    #random_forest_experiment(X, Y, kf)
+    #mlp_experiment(X, Y, kf)
+    plotarAllMetrics()
+
+    for i in range(len(all_metrics)):
+        plot_confusion_matrix(i,all_metrics[i][0])
+       #plot_confusion_matrix(all_confusion_matrix[i])
+       
+       
+
+
+def plotarAllMetrics():
+    df = pd.DataFrame(all_metrics, columns=['Modelo', 'Acurácia Média', 'Precisão Média', 'Revocação Média', 'F1-Score Médio'])
     
+    # Calcula o tamanho da figura com base no número de colunas e linhas
+    n_rows, n_cols = df.shape
+    fig_width = n_cols * 2  # Largura baseada no número de colunas
+    fig_height = n_rows * 0.5 + 1  # Altura baseada no número de linhas
+
+    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
+    ax.axis('tight')
+    ax.axis('off')
+
+    # Cria a tabela
+    table = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)  # Substitua 12 pelo tamanho de fonte desejado
+    table.auto_set_column_width(list(range(len(df.columns))))
+    plt.show()
+
+def plot_confusion_matrix(i,model_name):
+    # Seleciona a matriz de confusão pelo índice i
+    cm = all_confusion_matrix[i]
+    
+    # Cria uma figura e um eixo
+    fig, ax = plt.subplots()
+    
+    # Plota a matriz de confusão como um mapa de calor
+    sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues', ax=ax, cbar=False, 
+                xticklabels=['Confirmed', 'False Positive'], yticklabels=['Confirmed', 'False Positive'])
+    
+    # Configura os rótulos e o título
+    ax.set_xlabel('Predito')
+    ax.set_ylabel('Real')
+    ax.set_title('Matriz de Confusão -'+ str(model_name))  # Título opcionalmente pode incluir o índice do modelo
+    
+    # Exibe o gráfico
+    plt.show()
+def main():
+    initial_Count()  # Certifique-se de que essa função está definida
+    #acuracia_media()
+
+
+   
+    results()
     return 0
 
 if __name__ == '__main__':
